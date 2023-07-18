cef76c57e31861526c3bcb22e0dd692531e6d8e8
michael frederickson <mpfplus@gmail.com>
2023-07-18 16:37:32 -0600

    Completed tests for serializer

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,24 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Create serializer with start"""
+        self.start = start
+        self.current_num = start
+        self.firstGeneration = True
+
+    def generate(self):
+        """Returns the next sequential number, starting from start"""
+        if self.firstGeneration:
+            self.firstGeneration = False
+            return self.start
+        else:
+            self.current_num += 1
+            return self.current_num
+
+
+    def reset(self):
+        """Reset the number back to the original start"""
+        self.current_num = self.start
+        self.firstGeneration = True
